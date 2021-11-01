9a176e083abc162e383b9ffa34f726c846dd7fa3
cFadness <codie1989@gmail.com>
2021-11-01 11:39:33 -0700

    Created and exported variables from Account file and imported them to ATM file. Finished creating all functions on ATM file.

--- account.js
+++ account.js
@@ -1,4 +1,13 @@
 "use strict";
+
+let accountBalance = 500
+let accountPin = 1234
+
+module.exports = {
+    userAccountBalance: accountBalance,
+    userAccountPin: accountPin
+}
+
 //! START HERE! Then atm.js, then index.js
 //! Remember to run npm init to create your Node application!
 
--- atm.js
+++ atm.js
@@ -1,21 +1,35 @@
 "use strict";
+
+const {userAccountBalance, userAccountPin} = require("./account")
+
 //TODO: Import necessary values from account.js
 
 function getBalance() {
+  return userAccountBalance
   //TODO: Return the customer's acct. balance
 }
 
 function withdraw(withdrawAmount) {
+  let withdrawBalance = userAccountBalance - withdrawAmount
+  console.log(withdrawBalance)
   //TODO: withdraw amount from current acct. balance
   // Log the current balance after withdrawal is made
 }
 
 function deposit(depositAmount) {
+  let depositBalance = userAccountBalance + depositAmount
+  console.log(depositBalance)
   //TODO: deposit amount to current acct. balance
   // Log the current balance after deposit is made
 }
 
 function validatePin(enteredPin) {
+  if(enteredPin == userAccountPin){
+    return true
+  }
+  else{
+    return false
+  }
   //TODO: Check if entered pin matches account.js pin
   //Allow access to ATM if matching
   //Return value should be a Boolean (true or false)
